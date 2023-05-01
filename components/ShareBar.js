4d0231df5fe9fbb475d7b487856e09618a966734
RedhairHambagu <129669334+RedhairHambagu@users.noreply.github.com>
2023-05-01 17:30:46 +0800

    fix/sharebar useEffect justify-start end

--- components/ShareBar.js
+++ components/ShareBar.js
@@ -1,18 +1,44 @@
 import BLOG from '@/blog.config'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ShareButtons from './ShareButtons'
 
 const ShareBar = ({ post }) => {
   const router = useRouter()
+  const [justifyContent, setJustifyContent] = useState('start')
+  const shareBarRef = useRef(null)
 
+  useEffect(() => {
+    const handleResize = () => {
+      console.log('handleResize')
+      if (shareBarRef.current) {
+        const containerWidth = shareBarRef.current.offsetWidth
+        if (containerWidth < 520) {
+          setJustifyContent('start')
+        } else {
+          setJustifyContent('end')
+        }
+      }
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [shareBarRef])
+  
   if (!JSON.parse(BLOG.POST_SHARE_BAR_ENABLE) || !post || post?.type !== 'Post') {
     return <></>
   }
 
   const shareUrl = BLOG.LINK + router.asPath
 
-  return <div className='py-1 flex overflow-x-auto justify-start'>
+  return ((<div
+          ref={shareBarRef}
+          className="py-1 flex overflow-x-auto"
+          style={{ justifyContent }}
+        >
         <ShareButtons shareUrl={shareUrl} title={post.title} image={post.pageCover} body={
             post.title +
             ' | ' +
@@ -22,6 +48,6 @@ const ShareBar = ({ post }) => {
             ' ' +
             post.summary
         } />
-    </div>
+    </div>)
 }
 export default ShareBar
