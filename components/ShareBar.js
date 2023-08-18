3f4c75e82b4180aeef54243d86e6948910e54af3
RedhairHambagu <129669334+RedhairHambagu@users.noreply.github.com>
2023-08-18 08:30:54 +0000

    fix minor files

--- components/ShareBar.js
+++ components/ShareBar.js
@@ -1,32 +1,10 @@
 import BLOG from '@/blog.config'
 import { useRouter } from 'next/router'
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import ShareButtons from './ShareButtons'
 
 const ShareBar = ({ post }) => {
   const router = useRouter()
-  const [justifyContent, setJustifyContent] = useState('start')
-  const shareBarRef = useRef(null)
-
-  useEffect(() => {
-    const handleResize = () => {
-      console.log('handleResize')
-      if (shareBarRef.current) {
-        const containerWidth = shareBarRef.current.offsetWidth
-        if (containerWidth < 520) {
-          setJustifyContent('start')
-        } else {
-          setJustifyContent('end')
-        }
-      }
-    }
-
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [shareBarRef])
 
   if (!JSON.parse(BLOG.POST_SHARE_BAR_ENABLE) || !post || post?.type !== 'Post') {
     return <></>
